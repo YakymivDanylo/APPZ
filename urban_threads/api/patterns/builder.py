a8f34a40d8bd15dbe18ad10a86dfdb49b4299065
YakymivDanylo <dr.yakimiv@gmail.com>
2025-11-29 13:14:30 +0200

    first commit

---
+++ urban_threads/api/patterns/builder.py
@@ -0,0 +1,38 @@
+# patterns.py
+class OrderBuilder:
+    def __init__(self, user):
+        self.order = Order(user=user, status='draft')
+
+    def add_items_from_cart(self, cart_items):
+        # Логіка перенесення товарів з кошика в замовлення
+        self.items = cart_items
+        return self
+
+    def set_shipping_address(self, address):
+        self.order.shipping_address = address
+        return self
+
+    def set_payment_method(self, method):
+        self.order.payment_method = method
+        return self
+
+    def build(self):
+        self.order.save()
+        for item in self.items:
+            OrderItem.objects.create(order=self.order, product=item.product, qty=item.qty)
+        return self.order
+
+
+# views.py
+class CheckoutView(APIView):
+    def post(self, request):
+        builder = OrderBuilder(request.user)
+        cart_items = CartItem.objects.filter(user=request.user)
+
+        order = (builder
+                 .add_items_from_cart(cart_items)
+                 .set_shipping_address(request.data['address'])
+                 .set_payment_method(request.data['payment_method'])
+                 .build())
+
+        return Response({"order_id": order.id, "status": "Created"}, status=201)
