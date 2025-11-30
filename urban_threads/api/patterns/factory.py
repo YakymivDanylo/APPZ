fc9e74d510dd1df5d4832cd355cb9953d6ce8729
YakymivDanylo <dr.yakimiv@gmail.com>
2025-11-30 22:10:03 +0200

    just some changes

--- urban_threads/api/patterns/factory.py
+++ urban_threads/api/patterns/factory.py
@@ -4,6 +4,4 @@ class UserFactory:
     @staticmethod
     def create_user(username, email, password, role='customer'):
         user = User.objects.create_user(username=username, email=email, password=password)
-        # Тут можна додати логіку для створення розширених профілів (Manager, Admin)
-        # Наприклад: if role == 'manager': ManagerProfile.objects.create(user=user)
         return user
